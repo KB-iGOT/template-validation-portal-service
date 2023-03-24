725febcc6f03cf46eb9ed7aa65945299bc9fd570
githubpiyush <piyush.jain119981@gmail.com>
2023-03-24 08:44:09 +0530

    Added final project template

--- backend/src/main/modules/xlsxObject.py
+++ backend/src/main/modules/xlsxObject.py
@@ -112,13 +112,14 @@ class xlsxObject:
       rolesList.append(roles["code"])
     if len(rolesList) == 0:
       responseData["data"].append({"errCode":errAdv, "sheetName":sheetName,"columnName":columnName,"rowNumber":1,"errMessage":"recommendedFor role list is empty in the backend", "suggestion":"Please at least one role in the backend"})
-    
+      return responseData
     for idx, row in self.xlsxData[sheetName].iterrows():
       if idx > 1 and not multipleRow:
         break
-      df = [y.strip() for y in row[columnName].split(",")]
-      if not all(item in rolesList for item in df):
-        responseData["data"].append({"errCode":errAdv, "sheetName":sheetName,"columnName":columnName,"rowNumber":idx,"errMessage":conditionData["recommendedForCheck"]["errMessage"], "suggestion":conditionData["recommendedForCheck"]["suggestion"]})
+      if row[columnName] == row[columnName]:
+        df = [y.strip() for y in row[columnName].split(",")]
+        if not all(item in rolesList for item in df):
+          responseData["data"].append({"errCode":errAdv, "sheetName":sheetName,"columnName":columnName,"rowNumber":idx,"errMessage":conditionData["recommendedForCheck"]["errMessage"], "suggestion":conditionData["recommendedForCheck"]["suggestion"]})
                 
 
     return responseData
