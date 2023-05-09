032b63a2437dd43e289b9528c5b8c7940bbe54eb
adithya_dinesh <adithya@shikshalokam.org>
2023-05-09 17:42:09 +0530

    added host and port to app.run()

--- apiServices/src/main/app.py
+++ apiServices/src/main/app.py
@@ -695,5 +695,5 @@ def updateValidations(_id):
 
 
 if (__name__ == '__main__'):
-    host = os.environ.get("HOSTIP")
-    app.run(host=str(host) , port=os.environ.get("FLASK_RUN_PORT") , debug=True)
+
+    app.run(host=os.environ.get("HOSTIP")  , port=os.environ.get("FLASK_RUN_PORT") , debug=True)
